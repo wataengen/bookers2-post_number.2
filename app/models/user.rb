bff3bc53a2f54a24a7818f396099ea9bf37ccb76
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-01 07:05:29 +0000

    [Update]いいねの多い順に並び変える

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_one_attached :profile_image
