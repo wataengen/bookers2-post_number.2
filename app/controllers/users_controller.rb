38ddde842387d18ec9e2bd7f49c1e0772a6c0b7c
NakamuraWataru <130727655+wataengen@users.noreply.github.com>
2023-08-02 20:28:58 +0000

    [Update]7日分の投稿の集計

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,11 @@ class UsersController < ApplicationController
       end
     end
 
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    # @this_week_book = @books.created_this_week
+    # @last_week_book = @books.created_last_week
+
   end
 
   def index
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,6 +7,14 @@ class Book < ApplicationRecord
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
 
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_2day_ago, -> { where(created_at: 2.day.ago.all_day) } # 2日前
+  scope :created_3day_ago, -> { where(created_at: 3.day.ago.all_day) } # 3日前
+  scope :created_4day_ago, -> { where(created_at: 4.day.ago.all_day) } # 4日前
+  scope :created_5day_ago, -> { where(created_at: 5.day.ago.all_day) } # 5日前
+  scope :created_6day_ago, -> { where(created_at: 6.day.ago.all_day) } # 6日前
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
